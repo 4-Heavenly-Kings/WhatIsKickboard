c9fd6d32be0e9e319a70f2a99c52d7b69efbb86d
QuaRang <dbduddnd1225@gmail.com>
2025-04-26 18:26:03 +0900

    feat: #1 - UserPersistence 메서드 정의
    - CoreDataStack에서 만든 context에서 작업할 각각의 기능 정의

---
+++ WhatIsKickboard/Data/Manager/UserPersistenceManager.swift
@@ -0,0 +1,41 @@
+//
+//  Empty1.swift
+//  WhatIsKickboard
+//
+//  Created by 백래훈 on 4/25/25.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - UserPersistenceManager
+/// 유저정보 CRUD Manager
+final class UserPersistenceManager {
+    
+    static let context = CoreDataStack.shared.context
+    
+    /// 로그인
+    static func login(_ email: String, _ password: String) -> User {
+        return User(context: context)
+    }
+    
+    /// 회원가입
+    static func register(_ email: String, _ password: String) -> User {
+        return User(context: context)
+    }
+    
+    /// 유저정보 변경
+    static func patchUser(_ user: User) -> User {
+        return User(context: context)
+    }
+    
+    /// 로그아웃
+    static func logout() -> Bool {
+        return Bool()
+    }
+    
+    /// 회원탈퇴
+    static func delete() -> Bool{
+        return Bool()
+    }
+}
