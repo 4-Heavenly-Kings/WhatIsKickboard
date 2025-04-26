b86434572effc2d63313d50d2b88d8f3f3274ed6
QuaRang <dbduddnd1225@gmail.com>
2025-04-26 18:17:19 +0900

    feat: #1 - CoreDataStack 구현
    - Persistene Container 및 context 정의
    - context의 task는 background에서 동작해도 무관하기 때문에 backgroundContext로 정의

---
+++ WhatIsKickboard/Data/Core/CoreDataStack.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataStack.swift
+//  WhatIsKickboard
+//
+//  Created by 유영웅 on 4/26/25.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - CoreDataStack
+/// NSPersistentContainer 설정과  backgroundContext 제공
+final class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+
+    private init() { }
+
+    // Core Data의 전체 스택을 관리하는 컨테이너
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { _, error in
+            if let error { fatalError("컨테이너 로딩 실패: \(error)") }
+        }
+        return container
+    }()
+    
+    // Core Data의 작업 공간(CRUD)
+    var context: NSManagedObjectContext {
+        persistentContainer.newBackgroundContext()
+    }
+}
