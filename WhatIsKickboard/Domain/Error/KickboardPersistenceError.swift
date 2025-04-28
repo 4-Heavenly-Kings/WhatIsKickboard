b9607c094d4737dd22b4dbe2526b2f99308b98ed
QuaRang <dbduddnd1225@gmail.com>
2025-04-28 18:10:17 +0900

    feat: #5 - KickboardError 내용 추가
    - 킥보드 및 탑승정보 조회 실패 에러 타입 추거

--- WhatIsKickboard/Domain/Error/KickboardPersistenceError.swift
+++ WhatIsKickboard/Domain/Error/KickboardPersistenceError.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 enum KickboardPersistenceError: String, Error {
+    case userNotFound = "해당 유저가 존재하지 않습니다."
+    case tokenNotValid = "토큰이 유효하지 않습니다."
     case kickboardNotFound = "해당 킥보드가 존재하지 않습니다."
-    case fetchFaild = "킥보드 데이터 로딩 실패"
+    case rideNotFound = "해당 탑승정보가 존재하지 않습니다."
+    case fetchKickboardFaild = "킥보드 데이터 로딩 실패."
+    case fetchRideFaild = "탑승정보 데이터 로딩 실패."
 }
