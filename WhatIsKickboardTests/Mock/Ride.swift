85c4c4132dc17b354f203af4523b525a6f6a5cd6
QuaRang <dbduddnd1225@gmail.com>
2025-04-28 22:03:02 +0900

    feat: #5 - 킥보드 관련 테스트 코드 작성 및 목 정의
    - 킥보드 관련 메서드들을 테스트 하기 위한 테스트 코드 작성
    - 해당 데이터를 비교하기 위한 Mock 정의

---
+++ WhatIsKickboardTests/Mock/Ride.swift
@@ -0,0 +1,29 @@
+//
+//  Ride.swift
+//  WhatIsKickboardTests
+//
+//  Created by 유영웅 on 4/28/25.
+//
+
+import Foundation
+@testable import WhatIsKickboard
+
+// MARK: - KickboardRide Mock
+extension KickboardRide {
+    static func getMock(kickboardId: UUID, userId: UUID) -> Self {
+        return KickboardRide(
+            id: UUID(),
+            userId: userId,
+            kickboardId: kickboardId,
+            startTime: Date(),
+            endTime: nil,
+            startLatitude: 37.1235,
+            startLongitude: 127.1235,
+            endLatitude: 37.1236,
+            endLongitude: 127.1236,
+            battery: 50,
+            price: 500,
+            imagePath: "test/path"
+        )
+    }
+}
--- WhatIsKickboardTests/Mock/User.swift
+++ WhatIsKickboardTests/Mock/User.swift
@@ -8,6 +8,7 @@
 import Foundation
 @testable import WhatIsKickboard
 
+// MARK: - User Mock
 extension User {
     func getMock() -> Self{
         User(id: UUID(), email: "test@example.com", password: "password123", role: "GUEST")
