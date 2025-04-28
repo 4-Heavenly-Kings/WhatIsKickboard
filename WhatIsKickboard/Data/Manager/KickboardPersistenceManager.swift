af451aa968cefd7e21c2cb15b7da41c05b409a2c
QuaRang <dbduddnd1225@gmail.com>
2025-04-28 16:04:44 +0900

    feat: #5 - 킥보드 관련 메서드 정의
    - 삭제는 시간이 여유로울 때 구현할 예정

--- WhatIsKickboard/Data/Manager/KickboardPersistenceManager.swift
+++ WhatIsKickboard/Data/Manager/KickboardPersistenceManager.swift
@@ -12,4 +12,38 @@ import CoreData
 /// 킥보드정보 CRUD Manager
 final class KickboardPersistenceManager: BaseCoreDataManager {
     
+    /// 킥보드 리스트 조회
+    static func getKickboardList() -> [Kickboard] {
+        return []
+    }
+    
+    /// 킥보드 조희
+    static func getKickboard() -> Kickboard {
+        return KickboardEntity(context: context).toModel()
+    }
+    
+    /// 킥보드 등록
+    static func createKickboard() {
+        
+    }
+    
+    /// 킥보드 대여
+    static func rentKickboard() {
+        
+    }
+    
+    /// 킥보드 반납
+    static func returnKickboard() {
+        
+    }
+    
+    /// 킥보드 신고
+    static func declaredKickboard() {
+        
+    }
+    
+    /// 킥보드 삭제
+    static func deleteKickboard() {
+        
+    }
 }
