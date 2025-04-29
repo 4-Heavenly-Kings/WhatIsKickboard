1462c824494c6c54f7a2edeaa824bd4f57ff0b61
QuaRang <dbduddnd1225@gmail.com>
2025-04-29 17:20:15 +0900

    feat: #15 - 이름 수정 ViewModel 생성 및 비즈니스 설계
    - 마찬가지로 확인하기 버튼에 대한 비즈니스 로직밖에 존재하지 않음으로 액션은 하나만 사용

---
+++ WhatIsKickboard/Presentation/Onboarding/ViewModel/EditNameViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  EditNameViewModel.swift
+//  WhatIsKickboard
+//
+//  Created by 유영웅 on 4/29/25.
+//
+
+import Foundation
+import RxSwift
+
+// MARK: - EditNameViewModel
+final class EditNameViewModel: ViewModelProtocol {
+    
+    // MARK: - VC에서 방출하는 이벤트 액션 정의
+    enum Action {
+        case didTapConfirm
+    }
+    
+    // MARK: - 상태관리 프로퍼티
+    struct State {
+        fileprivate(set) var actionSubject = PublishSubject<Action>()
+    }
+    
+    // MARK: - Action -> Observer 생성
+    var action: AnyObserver<Action> {
+        state.actionSubject.asObserver()
+    }
+    
+    var disposeBag = DisposeBag()
+    var state = State()
+    
+    // MARK: - init시 이벤트에 따라 메서드 실행
+    init() {
+        state.actionSubject
+            .subscribe(with: self){ owner, action in
+                switch action {
+                case .didTapConfirm: owner.confirm()
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func confirm() {
+        
+    }
+}
