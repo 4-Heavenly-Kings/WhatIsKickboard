c652cff72652ef114b6112f07622653c9451ce70
QuaRang <dbduddnd1225@gmail.com>
2025-04-29 10:18:14 +0900

    feat: #7 - 구현 UI ViewController 정의
    - 스플래시 뷰 추가

---
+++ WhatIsKickboard/Presentation/Onboarding/View/SplashView.swift
@@ -0,0 +1,15 @@
+//
+//  SplashView.swift
+//  WhatIsKickboard
+//
+//  Created by 유영웅 on 4/29/25.
+//
+
+import UIKit
+
+final class SplashView: BaseView {
+    
+    private let titleView = UIImageView()
+    private let titleImageView = UIImageView()
+    
+}
---
+++ WhatIsKickboard/Presentation/Onboarding/ViewController/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  WhatIsKickboard
+//
+//  Created by 유영웅 on 4/29/25.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+    
+    
+    
+    override func loadView() {
+        // 뷰 초기화
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func bindViewModel() {
+        <#code#>
+    }
+
+    override func setStyles() {
+        <#code#>
+    }
+}
