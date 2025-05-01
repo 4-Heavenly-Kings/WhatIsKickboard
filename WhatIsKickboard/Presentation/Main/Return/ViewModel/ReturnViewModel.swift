d1c3898d1219e465352032c0a4a36448df87f62a
cjs1399 <cjs1399@naver.com>
2025-05-01 09:27:45 +0900

    feat: #19 - add ViewModel

--- WhatIsKickboard/Presentation/Main/Return/ViewModel/ReturnViewModel.swift
+++ WhatIsKickboard/Presentation/Main/Return/ViewModel/ReturnViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxRelay
+
+final class ReturnViewModel: ViewModelProtocol {
+
+    enum Action {
+
+    }
+
+    struct State {
+
+    }
+
+    private let actionSubject = PublishSubject<Action>()
+    
+    var action: AnyObserver<Action> { actionSubject.asObserver() }
+
+    let state = State()
+    let disposeBag = DisposeBag()
+
+    init() {
+        bind()
+    }
+
+    private func bind() {
+
+    }
+    
+
+}
