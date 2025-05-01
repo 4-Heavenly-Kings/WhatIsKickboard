47935ccc9f2f31691c87d1a87f0f65ffec5e1b23
QuaRang <dbduddnd1225@gmail.com>
2025-05-01 16:58:13 +0900

    refactor: #33 - 유즈 케이스 인터페이스 정의
    - User 및 Kickboard 관련 유즈케이스

---
+++ WhatIsKickboard/Domain/UseCase Interface/UserPersistenceUseCaseInterface.swift
@@ -0,0 +1,24 @@
+//
+//  UserPersistenceUseCaseInterface.swift
+//  WhatIsKickboard
+//
+//  Created by 유영웅 on 5/1/25.
+//
+
+import Foundation
+import RxSwift
+
+protocol UserPersistenceUseCaseInterface {
+    /// 로그인
+    static func login(_ email: String, _ password: String) -> Single<User>
+    /// 회원가입
+    static func createUser(_ email: String, _ password: String) -> Single<User>
+    /// 유저정보 조회
+    static func getUser() -> Single<User>
+    /// 유저정보 변경
+    static func patchUser(_ user: User) -> Single<User>
+    /// 로그아웃
+    static func logout()
+    /// 회원탈퇴
+    static func deleteUser(password: String) async throws
+}
